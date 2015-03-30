3b1ebf5fd7aa7a5b67fc20b0ec6ff33eccfa5185
J. Antognini <antognini@astronomy.ohio-state.edu>
2015-03-30 12:54:32 -0400

    Have the overall algorithm in place, still bugs

--- 061/figuratives.py
+++ 061/figuratives.py
@@ -9,11 +9,11 @@
 #
 
 # First generate the figurative numbers
-N = 120 # Some large number...
+N = 150 # Some large number...
 lb = 1000
 ub = 10000
 triangles = [n*(n+1)/2 for n in range(N)]
-triangles = [elem for elem in triangels if lb <= elem < ub]
+triangles = [elem for elem in triangles if lb <= elem < ub]
 squares = [n*n for n in range(N)]
 squares = [elem for elem in squares if lb <= elem < ub]
 pentagons = [n*(3*n-1)/2 for n in range(N)]
@@ -47,13 +47,34 @@ def find_index(lst, n):
   lstlen = len(lst)
   if lstlen <= 1:
     return 0
-  if lst[lstlen/2] > n:
+  if split_integer(lst[lstlen/2])[0] > n:
     return find_index(lst[:lstlen/2], n)
   else:
     return find_index(lst[lstlen/2:], n) + lstlen/2
 
+def next_link(cur_figures, prev, first_half):
+  if len(cur_figures) == 0 and prev == first_half:
+    return True
+  elif len(cur_figures) == 0 and prev != first_half:
+    return False
+
+  # Find the next link
+  for figure in cur_figures:
+    i = find_index(figure, prev)
+    other_figures = [elem for elem in cur_figures if elem != figure]
+    while split_integer(figure[i])[0] == prev:
+      next = split_integer(figure[i])[1]
+      print figure[i]
+      if next_link(other_figures, next, first_half):
+        print figure[i]
+        return True
+      i += 1  
+
+  return False
+
 for elem in triangles:
   prev, next = split_integer(elem)
-  figure = figures.pop()
-  for 
-      
+  print 'triangle:', elem
+  if next_link(figures, next, prev):
+    print elem
+    break
